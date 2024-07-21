aa6e069cf21240c1f9f279e758394b3ded243014
Stiliyan Nanovski <S.I.Nanovski@student.tudelft.nl>
2024-07-21 13:45:45 +0300

    Split project structure into 2 subprojects - one for the mad4j library and one for integration tests with end-to-end test.

---
+++ test-bots/general-bot/src/main/java/net/stelitop/generalbot/GeneralbotApplication.java
@@ -0,0 +1,13 @@
+package net.stelitop.generalbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralbotApplication.class, args);
+	}
+
+}
---
+++ test-bots/general-bot/src/test/java/net/stelitop/generalbot/GeneralbotApplicationTests.java
@@ -0,0 +1,13 @@
+package net.stelitop.generalbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
